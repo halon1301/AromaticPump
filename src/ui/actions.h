c4e7ff0bd0170abbe9e30935ad11e95c9db98977
Lee Holder <lee.holder@fxstech.net>
2024-04-28 12:18:54 -0400

    Adding Basic UI Elements and controls
    Adds in all the UI elements and controls for basic functionality.

---
+++ src/ui/actions.h
@@ -0,0 +1,10 @@
+#ifndef EEZ_LVGL_UI_ACTIONS_H
+#define EEZ_LVGL_UI_ACTIONS_H
+
+#include "lvgl.h"
+#include <Arduino.h>
+
+extern void action_onclick(lv_event_t * e);
+
+
+#endif /*EEZ_LVGL_UI_ACTIONS_H*/
